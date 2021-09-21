0094166e7a1c7b8480c2d687867d7e8a07a8a56c
Lando Icaza <landoicco@gmail.com>
2021-09-21 14:39:10 -0500

    Database updated!!

--- src/main/java/lando/spring/payroll/LoadDatabase.java
+++ src/main/java/lando/spring/payroll/LoadDatabase.java
@@ -12,10 +12,20 @@ public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository,
+                                   OrderRepository orderRepository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Hazel",  "Olvera", "Physicist")));
-            log.info("Preloading " + repository.save(new Employee("Leo", "Infante", "Mathematician")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Hazel", "Olvera", "Physicist")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Leo", "Infante", "Mathematician")));
+
+            employeeRepository.findAll().forEach(employee ->
+                    log.info("Preloaded " + employee));
+
+            orderRepository.save(new Order("Mac Book Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+            orderRepository.findAll().forEach(order -> {
+                log.info(("Preloaded " + order));
+            });
         };
     }
 
